03581fb66143924e918b86fbb00146599f07af50
ldakhoa <ldakhoa1308@gmail.com>
2022-06-10 16:33:09 +0700

    Initial Commit

---
+++ Memosdotpub/MemosdotpubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemosdotpubApp.swift
+//  Memosdotpub
+//
+//  Created by Khoa Le on 10/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemosdotpubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
